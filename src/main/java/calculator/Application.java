1df042e99aa22cdc7e7a1ac1c6f8e79ff8396d9c
윤현기 <dbsgusrl1234567@naver.com>
2025-10-15 10:58:38 +0900

    test: console API 테스트

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-
+        System.out.println("입력 : ");
+        String input = Console.readLine();
+        System.out.println("출력 값 :" + input);
     }
 }
