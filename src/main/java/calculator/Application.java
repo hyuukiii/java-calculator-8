2aa0f20280bec593717f932b0a15f7c2c9f7cdd7
윤현기 <dbsgusrl1234567@naver.com>
2025-10-15 12:43:47 +0900

    feat - 쉼표,콜론,세미콜론 문자열 분리 기능 구현 - 분리된 문자열 숫자로 변환 구현 - 입력 받은 숫자, 구분자에 따라 합산 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,8 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("입력 : ");
-        String input = Console.readLine();
-        System.out.println("출력 값 :" + input);
+        System.out.println("더할 숫자를 입력하세요 : ");
+        String input = Console.readLine(); // 1 2 3 입력
+
+        // "1, 2, 3" ==> 을 구분자에 따라 다른 문자열 변환
+        String[] numbers = input.split("[,:;]") ;
+
+        // 반복문 밖에서 초기화 하기
+        int parseSum = 0;
+
+        for (String number : numbers) {
+            int parseNums = Integer.parseInt(number);
+            parseSum += parseNums;
+        }
+
+        System.out.println("합계 결과 :" + parseSum);
     }
 }
